592ff23be2391858af88b9fde83d4aca9f5b9953
rcharles <rc1522@ic.ac.uk>
2025-08-20 12:26:45 +0100

    Updated version and .toml package

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ from .extras import (
     printblank, printheader, printtable, printlist
 )
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "printwork", "printok", "printfail", "printwarn", 
     "printdone", "printinfo", "printdebug",
